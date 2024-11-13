71c709dffa82a5e3042f9d2535ebe4377de669aa
Murshidmuhammedp <murshid.mmp@gmail.com>
2024-11-13 20:13:24 +0530

    BaseURL

--- src/confiq/axios.js
+++ src/confiq/axios.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 
 export const customAxios = axios.create({
-    baseURL: "http://localhost:3456"
+    baseURL: "https://book-app-back-end-qhr8.onrender.com"
+    // baseURL: "http://localhost:3456"
 });
